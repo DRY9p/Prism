bc5a8d645ae71df1570fc2d41ab743d6d2cfe455
Cr9Co0 <alburton32@gmail.com>
2026-05-21 12:13:02 +0500

    Добавлены паршалы _auth_fields и _flash в home/index. Поправить расстояние между кнопкой и текстом в registration/new

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -4,6 +4,7 @@ class RegistrationController < ApplicationController
   rate_limit to: 10, within: 3.minutes, only: :create, with: -> {redirect_to new_session_url, alert: "Try again later."}
   
   def new
+    @user = User.new
     redirect_to root_url, notice: "Вы уже авторизованы" if authenticated?
   end
 
