72e3520b5fa4ab11f638ed0440f0f2409b414ea0
Cr9Co0 <alburton32@gmail.com>
2026-05-22 14:55:15 +0500

    feat(registration): стилизация формы, улучшение обратной связи и маршрутизация
    - Добавлено стилизованное поле :password_confirmation во view/registration/new.html.erb
    - В partial shared/flash добавлена пульсирующая анимация уведомлений
    - Обновлена кнопка «Войти» в shared/navigation (визуальные правки)
    - Контроллер registration_controller:
      * реорганизована логика
      * выделен приватный метод user_params для строгой типизации параметров
    - В routes.rb ограничены доступные экшены:
      * resources :sessions, only: %i[new create destroy]
      * resources :passwords, only: %i[new create edit update]

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -4,15 +4,24 @@ class RegistrationController < ApplicationController
   rate_limit to: 10, within: 3.minutes, only: :create, with: -> {redirect_to new_session_url, alert: "Try again later."}
   
   def new
-    @user = User.new
     redirect_to root_url, notice: "Вы уже авторизованы" if authenticated?
   end
 
   def create
-    user = User.new(params.permit(:email_address, :password))
+    user = User.new(user_params)
+
     if user.save
       start_new_session_for user
-      redirect_to after_authentication_url, notice: "Вы успешно зарегестрировались"
+      # redirect_to after_authentication_url, notice: "Вы успешно зарегестрировались"
+      redirect_to new_session_path, notice: "Вы успешно зарегестрировались"
+    else
+      redirect_to new_registration_path, notice: user.errors.full_messages
     end
+  end
+
+  private
+
+  def user_params
+    params.permit(:email_address, :password, :password_confirmation) 
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "home#index"
 
-  resource :session
-  resources :passwords, param: :token
+  resource :session, only: %i[new create destroy]
+  resources :passwords, only: %i[new create edit update], param: :token
   resources :registration, only: %i[new create]
 end
